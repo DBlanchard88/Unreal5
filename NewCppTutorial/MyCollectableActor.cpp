04fe246e57efc394b40a4612c5f4ab907e7a9b77
DBlanchard88 <davidblanchard88@gmail.com>
2024-10-10 13:42:55 -0400

    added collision

---
+++ NewCppTutorial/MyCollectableActor.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCollectableActor.h"
+
+// Sets default values
+AMyCollectableActor::AMyCollectableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create the (root) component for rendering
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	RootComponent = StaticMesh;
+
+	// Create collision box
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetupAttachment(StaticMesh);
+
+}
+
+void AMyCollectableActor::Jump(float velocity)
+{
+	// Check if object has been launched only once
+	if (!IsLaunched)
+	{
+		// Execute jump using physics system
+		StaticMesh->AddImpulse({ .0f, .0f, velocity * 500.f });
+
+		// Initiate object destruction
+		SetActorTickEnabled(true);
+		IsLaunched = true;
+	}
+
+}
+
+// Called when the game starts or when spawned
+void AMyCollectableActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Setup per instance OncomponentOverlap Event
+	FScriptDelegate DelegateSubscriber;
+	DelegateSubscriber.BindUFunction(this, "OnComponentBeginOverlap");
+	BoxCollision->OnComponentBeginOverlap.Add(DelegateSubscriber);
+
+	// Ticking is only required after launching
+	SetActorTickEnabled(false);
+	
+}
+
+void AMyCollectableActor::OnComponentBeginOverlap(UBoxComponent* Component, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!IsLaunched && OtherActor->IsA(TriggerClass))
+	{
+		OnJumpTrigger.Broadcast(OtherActor, Component);
+	}
+}
+
+// Called every frame
+void AMyCollectableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (IsLaunched)
+	{
+		//Decrement Livetime
+		Livetime -= DeltaTime;
+
+		// Check actor destruction
+		if (Livetime <= 0.f)
+		{
+			Destroy();
+		}
+	}
+
+}
+
